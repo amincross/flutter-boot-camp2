0e97e5321656b2f1cc96c9b4ec73490b59905c4b
Amin <amincross@yahoo.com>
2024-01-04 15:51:36 +0330

    first commit

--- flutter_boot_camp2/lib/splash_page.dart
+++ flutter_boot_camp2/lib/splash_page.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  Color buttonTextColor = Colors.yellow;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("This is the DATA!!!!"),
+      appBar: AppBar(backgroundColor: Colors.purple),
+      body: Image.network(
+        "https://images.squarespace-cdn.com/content/v1/60f1a490a90ed8713c41c36c/1629223610791-LCBJG5451DRKX4WOB4SP/37-design-powers-url-structure.jpeg",
+      ),
     );
   }
 }
