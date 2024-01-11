f6bda0f562aae7a11cb82bc8738646396fc82a0d
Amin <amincross@yahoo.com>
2024-01-11 15:46:58 +0330

    first commit

--- flutter_boot_camp2/lib/splash_page.dart
+++ flutter_boot_camp2/lib/splash_page.dart
@@ -8,60 +8,79 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  Color buttonTextColor = Colors.yellow;
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Container(
-          width: size.width,
-          height: size.height,
-          child: Column(
+        backgroundColor: Colors.grey,
+        body: Container(
+          margin: EdgeInsets.only(left: 36,right: 36),
+
+      width: size.width,
+      height: size.height,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          SizedBox(height: 50),
+
+          Align(
+            alignment: Alignment.center,
+              child: Text("SignUP",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 24),)),
+
+          Text("UserName",style: TextStyle(color: Colors.white,fontSize: 16),),
+          TextField(),
+
+          SizedBox(height: 25),
+
+          Text("Email",style: TextStyle(color: Colors.white,fontSize: 16),),
+          TextField(),
+
+          SizedBox(height: 25),
+
+          Text("Password",style: TextStyle(color: Colors.white,fontSize: 16),),
+          TextField(),
+
+
+          SizedBox(height: 25),
+          Text("Confirm Passwrod",style: TextStyle(color: Colors.white,fontSize: 16),),
+          TextField(),
+
+          SizedBox(height: 25),
+
+          Container(
+            width: size.width,
+              child: ElevatedButton(onPressed: (){}, child: Text("SignUP"))),
+
+          SizedBox(height: 25),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Sign UP!"),
-              Text("Please fill all fields"),
 
-              SizedBox(height: 50),
+              Container(width: 150,height: 1,color: Colors.black),
+              Text("Or Sign Up With"),
+              Container(width: 150,height: 1,color: Colors.black),
 
-              Container(
-                color: Colors.grey,
-                width: 250,
-                child: Column(
-                  children: [Text("Name: "), TextField()],
-                ),
-              ),
-
-              SizedBox(height: 80),
-
-              Container(
-                color: Colors.grey,
-                width: 250,
-                child: Column(
-                  children: [Text("Family: "), TextField()],
-                ),
-              ),
-
-              SizedBox(height: 80),
-
-              Container(
-                color: Colors.grey,
-                width: 250,
-                child: Column(
-                  children: [Text("Age: "), TextField()],
-                ),
-              ),
-
-              Row(
-                children: [
-                  ElevatedButton(onPressed: () {}, child: Text("Register")),
-                  Expanded(child: Container()),
-                  ElevatedButton(onPressed: () {}, child: Text("cancel")),
-                ],
-              )
             ],
-          )),
-    );
+          ),
+
+          SizedBox(height: 25),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(color: Colors.greenAccent,width: 100,height: 50,child: Icon(Icons.abc)),
+
+              Container(color: Colors.greenAccent,width: 100,height: 50,child: Icon(Icons.account_box_rounded)),
+
+              Container(color: Colors.greenAccent,width: 100,height: 50,child: Icon(Icons.accessible_rounded)),
+            ],
+          )
+
+        ],
+      ),
+    ));
   }
 }
